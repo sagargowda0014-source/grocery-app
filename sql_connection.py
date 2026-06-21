a523f38e04434702a0d0b204e84eee6e95aad0a7
sagargowda0014 <sagargowda0014@gmail.com>
2026-06-21 15:06:54 +0530

    fix sql_connection env vars

--- sql_connection.py
+++ sql_connection.py
@@ -3,14 +3,16 @@ import mysql.connector
 
 __cnx = None
 
+import os
+
 def get_sql_connection():
     global __cnx
     if __cnx is None:
         __cnx = mysql.connector.connect(
-            user='root',
-            password='Sgrnvn@14',
-            host='127.0.0.1',
-            database='grocery_store'
+            user=os.environ.get('MYSQLUSER'),
+            password=os.environ.get('MYSQLPASSWORD'),
+            host=os.environ.get('MYSQLHOST'),
+            database=os.environ.get('MYSQLDATABASE'),
+            port=int(os.environ.get('MYSQLPORT', 3306))
         )
     return __cnx
-
